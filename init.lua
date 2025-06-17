44e2eb0a85358db66dbf714b17244cd5cf0f70af
Arteposok <aeposokhov@gmail.com>
2025-06-17 12:45:38 +0400

    added a keymap to save and automatic nvim-tree.lua

--- init.lua
+++ init.lua
@@ -199,6 +199,14 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>fs', '<cmd>w<CR>', { desc = 'Save the opened file with :w' })
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    require('nvim-tree.api').tree.open()
+  end,
+})
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -254,7 +262,7 @@ require('lazy').setup({
     },
     config = function()
       require('nvim-tree').setup()
-      vim.keymap.set('n', '<leader>f', ':NvimTreeToggle<CR>', { desc = 'Toggle File Tree' })
+      vim.keymap.set('n', '<leader>ft', ':NvimTreeToggle<CR>', { desc = 'Toggle File Tree' })
     end,
   },
   {
