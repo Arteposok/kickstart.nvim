7be95b354d0e9d73b4b840ad250cd384d34ca938
Arteposok <aeposokhov@gmail.com>
2025-07-22 21:16:43 +0300

    m

--- init.lua
+++ init.lua
@@ -116,7 +116,7 @@ vim.o.showmode = false
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
-vim.g.python3_host_prog = vim.fn.system('uv python find --system'):gsub('%s+$', '')
+vim.g.python3_host_prog = vim.fn.system('uv python find'):gsub('%s+$', '')
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -231,7 +231,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
+vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
   pattern = '*',
   callback = function()
     if vim.bo.modified then
@@ -694,22 +694,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
         pyright = {
-          --on_init = function(client)
-          --  vim.fn.jobstart('uv python find', {
-          --    cwd = vim.fn.getcwd(),
-          --    on_stdout = function(jobid, data, event)
-          --      local python_path = table.concat(data, ''):gsub('%s+$', '')
-          --      client.config.settings.python.pythonPath = data
-          --      vim.g.python3_host_prog = data
-          --    end,
-          --  })
-          --end,
           python = {
-            pythonPath = vim.fn.system('uv python find --system'):gsub('%s+$', ''),
-            -- Fallback to .venv if uv fails
+            pythonPath = vim.fn.system('uv python find'):gsub('%s+$', ''),
+            venvPath = vim.fn.system('uv python find'):gsub('%s+$', ''),
             analysis = {
               autoSearchPaths = true,
               diagnosticMode = 'openFilesOnly',
@@ -736,16 +724,6 @@ require('lazy').setup({
           settings = {},
         },
         eslint = {},
-
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
