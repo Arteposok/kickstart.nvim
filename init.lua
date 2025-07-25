2d13a1cbb9d24445145282c4c05fa2a368ead837
Artem Posokhov <aeposokhov@gmail.com>
2025-07-25 20:24:49 +0300

    some tweaks again

--- init.lua
+++ init.lua
@@ -101,7 +101,7 @@ vim.g.have_nerd_font = true
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -150,8 +150,8 @@ vim.o.splitbelow = true
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.tabstop = 2 -- number of visual spaces per TAB
+vim.opt.listchars = { tab = '| ', trail = '·', nbsp = '␣' } -- »
+vim.opt.tabstop = 4 -- number of visual spaces per TAB
 vim.opt.shiftwidth = 2 -- spaces used for each step of (auto)indent
 vim.opt.expandtab = true -- convert tabs to spaces
 
@@ -162,7 +162,7 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.o.scrolloff = 7
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
