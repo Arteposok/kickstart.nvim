3a32fd7acd0821c669b036746532a0382b0d0569
Arteposok <aeposokhvo@gmail.com>
2025-08-28 22:04:03 +0400

    added lensline

--- lua/custom/plugins/tokyo-night.lua
+++ lua/custom/plugins/tokyo-night.lua
@@ -1,13 +1,27 @@
+local subtle_color = '#8c98b3' -- default comment and line numbers for tokyo-nights is too faded
+
 return {
   'folke/tokyonight.nvim',
-  name = 'tokyonight',
-  priority = 1000,
+  priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
+    ---@diagnostic disable-next-line: missing-fields
     require('tokyonight').setup {
+      on_colors = function(colors)
+        colors.comment = subtle_color
+      end,
+      on_highlights = function(hl, colors)
+        hl.LineNr = { fg = subtle_color }
+        hl.LineNrAbove = { fg = subtle_color }
+        hl.LineNrBelow = { fg = subtle_color }
+
+        -- Make Visual mode stand out more
+        hl.Visual = { bg = '#445b9b' }
+      end,
       styles = {
-        comments = { italic = false }, -- Disable italics in comments
+        comments = { italic = false }, -- copilot ghost text will use italic, to diffrentiate it from comments
       },
     }
-    vim.cmd.colorscheme 'catppuccin-mocha' --'github_dark_default'
+
+    vim.cmd.colorscheme 'tokyonight-night'
   end,
 }
